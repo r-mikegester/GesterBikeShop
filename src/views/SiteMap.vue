692733432cd3534c8c4c2dc9f17e6306f74ef87d
Mike Gester <mikegester.sabuga023@gmail.com>
2023-05-18 03:11:43 +0800

    added for quick viewing of links and navigation

--- src/views/SiteMap.vue
+++ src/views/SiteMap.vue
@@ -16,89 +16,189 @@
         </ion-toolbar>
       </ion-header>
 
-      <div id="container" class="bg-white">
-        <div class="dark:bg-gray-800 dark:text-gray-100">
-		<div class="px-4 py-16 mx-auto max-w-7xl sm:px-6 lg:px-8">
-			<div class="max-w-3xl mx-auto text-center">
-				<h2 class="text-3xl font-extrabold sm:text-4xl">All the features you want</h2>
-				<p class="mt-4 text-lg dark:text-gray-400">Pellentesque viverra, leo id euismod laoreet, nunc risus molestie orci, vel faucibus quam justo id mauris.</p>
-			</div>
-			<dl class="mt-12 space-y-10 sm:space-y-0 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 lg:grid-cols-4 lg:gap-x-8">
-				<div class="flex">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 dark:text-violet-400">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
-					</svg>
-					<div class="ml-3">
-						<dt class="text-lg font-medium">Quisque at urna</dt>
-						<dd class="mt-2 dark:text-gray-400">Vivamus ultricies bibendum tortor, molestie imperdiet justo cursus eu. Donec quis arcu magna. Integer tempor egestas dolor.</dd>
-					</div>
-				</div>
-				<div class="flex">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 dark:text-violet-400">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
-					</svg>
-					<div class="ml-3">
-						<dt class="text-lg font-medium">Quisque eu dui lacinia</dt>
-						<dd class="mt-2 dark:text-gray-400">Quisque ultricies volutpat sapien nec varius. Sed sit amet justo vestibulum, efficitur risus quis, gravida libero.</dd>
-					</div>
-				</div>
-				<div class="flex">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 dark:text-violet-400">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
-					</svg>
-					<div class="ml-3">
-						<dt class="text-lg font-medium">Mauris dignissim</dt>
-						<dd class="mt-2 dark:text-gray-400">Phasellus nec molestie arcu. Proin dictum, lorem mollis rutrum efficitur, lectus velit pharetra elit, et dictum purus nibh auctor velit.</dd>
-					</div>
-				</div>
-				<div class="flex">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 dark:text-violet-400">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
-					</svg>
-					<div class="ml-3">
-						<dt class="text-lg font-medium">Proin nulla eros</dt>
-						<dd class="mt-2 dark:text-gray-400">Sed ornare ultricies gravida. Morbi egestas dolor turpis, ornare laoreet est vehicula non.</dd>
-					</div>
-				</div>
-				<div class="flex">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 dark:text-violet-400">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
-					</svg>
-					<div class="ml-3">
-						<dt class="text-lg font-medium">Proin dictum</dt>
-						<dd class="mt-2 dark:text-gray-400">Lorem mollis rutrum efficitur, lectus velit pharetra elit, et dictum purus nibh auctor velit.</dd>
-					</div>
-				</div>
-				<div class="flex">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 dark:text-violet-400">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
-					</svg>
-					<div class="ml-3">
-						<dt class="text-lg font-medium">Quisque eu dui lacinia</dt>
-						<dd class="mt-2 dark:text-gray-400">Cras a mauris tincidunt, scelerisque justo sit amet, hendrerit est. Fusce venenatis diam fringilla metus convallis, at dapibus enim congue.</dd>
-					</div>
-				</div>
-				<div class="flex">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 dark:text-violet-400">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
-					</svg>
-					<div class="ml-3">
-						<dt class="text-lg font-medium">Cras vel bibendum tellus</dt>
-						<dd class="mt-2 dark:text-gray-400">Curabitur molestie neque ac massa pulvinar, nec sollicitudin nunc consequat. Donec mattis orci eros, vitae porttitor risus pretium eget.</dd>
-					</div>
-				</div>
-				<div class="flex">
-					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" class="flex-shrink-0 w-6 h-6 dark:text-violet-400">
-						<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 13l4 4L19 7"></path>
-					</svg>
-					<div class="ml-3">
-						<dt class="text-lg font-medium">Dignissim magna</dt>
-						<dd class="mt-2 dark:text-gray-400">Cras ac lectus erat. Curabitur condimentum luctus nisi, non lacinia ipsum.</dd>
-					</div>
-				</div>
-			</dl>
-		</div>
-	</div>
+      <div id="container" class="bg-white dark:bg-gray-900 h-full py-20">
+     <!-- Card Section -->
+<div class="max-w-[85rem] container px-4 py-5 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+  <!-- Grid -->
+  <div class="grid sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4 sm:gap-6">
+    <!-- Card -->
+    <a class="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800" href="/Home">
+      <div class="p-4 md:p-5">
+        <div class="flex justify-between items-center text-left">
+          <div>
+            <h3 class="group-hover:text-blue-600 text-left font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-200">
+              Home
+            </h3>
+            <p class="text-sm text-gray-500">
+              Client Side Page
+            </p>
+          </div>
+          <div class="pl-3">
+            <svg class="w-3.5 h-3.5 text-gray-500" width="16" height="16" viewBox="0 0 16 16" fill="none">
+              <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </a>
+    <!-- End Card -->
+
+    <!-- Card -->
+    <a class="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800" href="/Services">
+      <div class="p-4 md:p-5">
+        <div class="flex justify-between items-center text-left">
+          <div>
+            <h3 class="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-200">
+              Services
+            </h3>
+            <p class="text-sm text-gray-500">
+				Client Side Page
+            </p>
+          </div>
+          <div class="pl-3">
+            <svg class="w-3.5 h-3.5 text-gray-500" width="16" height="16" viewBox="0 0 16 16" fill="none">
+              <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </a>
+    <!-- End Card -->
+
+    <!-- Card -->
+    <a class="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800" href="/Contact">
+      <div class="p-4 md:p-5">
+        <div class="flex justify-between items-center text-left">
+          <div>
+            <h3 class="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-200">
+              Contact Us
+            </h3>
+            <p class="text-sm text-gray-500">
+				Client Side Page
+            </p>
+          </div>
+          <div class="pl-3">
+            <svg class="w-3.5 h-3.5 text-gray-500" width="16" height="16" viewBox="0 0 16 16" fill="none">
+              <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </a>
+    <!-- End Card -->
+
+    <!-- Card -->
+    <a class="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800" href="/About">
+      <div class="p-4 md:p-5">
+        <div class="flex justify-between items-center text-left">
+          <div>
+            <h3 class="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-200">
+              About us 
+            </h3>
+            <p class="text-sm text-gray-500">
+				Client Side Page
+            </p>
+          </div>
+          <div class="pl-3">
+            <svg class="w-3.5 h-3.5 text-gray-500" width="16" height="16" viewBox="0 0 16 16" fill="none">
+              <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </a>
+    <!-- End Card -->
+
+
+    <!-- Card -->
+    <a class="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800" href="/Admin">
+      <div class="p-4 md:p-5">
+        <div class="flex justify-between items-center text-left">
+          <div>
+            <h3 class="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-200">
+              Dashboard
+            </h3>
+            <p class="text-sm text-gray-500">
+              Admin Side Page
+            </p>
+          </div>
+          <div class="pl-3">
+            <svg class="w-3.5 h-3.5 text-gray-500" width="16" height="16" viewBox="0 0 16 16" fill="none">
+              <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </a>
+    <!-- End Card -->
+
+    <!-- Card -->
+    <a class="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800" href="/Messages">
+      <div class="p-4 md:p-5">
+        <div class="flex justify-between items-center text-left">
+          <div>
+            <h3 class="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-200">
+              Messages
+            </h3>
+            <p class="text-sm text-gray-500">
+				Admin Side Page
+            </p>
+          </div>
+          <div class="pl-3">
+            <svg class="w-3.5 h-3.5 text-gray-500" width="16" height="16" viewBox="0 0 16 16" fill="none">
+              <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </a>
+    <!-- End Card -->
+	   <!-- Card -->
+	   <a class="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800" href="/Clients">
+      <div class="p-4 md:p-5">
+        <div class="flex justify-between items-center text-left">
+          <div>
+            <h3 class="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-200">
+              Clients
+            </h3>
+            <p class="text-sm text-gray-500">
+				Admin Side Page
+            </p>
+          </div>
+          <div class="pl-3">
+            <svg class="w-3.5 h-3.5 text-gray-500" width="16" height="16" viewBox="0 0 16 16" fill="none">
+              <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </a>
+    <!-- End Card -->
+	   <!-- Card -->
+	   <a class="group flex flex-col bg-white border shadow-sm rounded-xl hover:shadow-md transition dark:bg-slate-900 dark:border-gray-800" href="/Notifications">
+      <div class="p-4 md:p-5">
+        <div class="flex justify-between items-center text-left">
+          <div>
+            <h3 class="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-gray-400 dark:text-gray-200">
+            Notifications
+            </h3>
+            <p class="text-sm text-gray-500">
+				Admin Side Page
+            </p>
+          </div>
+          <div class="pl-3">
+            <svg class="w-3.5 h-3.5 text-gray-500" width="16" height="16" viewBox="0 0 16 16" fill="none">
+              <path d="M5.27921 2L10.9257 7.64645C11.1209 7.84171 11.1209 8.15829 10.9257 8.35355L5.27921 14" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
+            </svg>
+          </div>
+        </div>
+      </div>
+    </a>
+    <!-- End Card -->
+  </div>
+  <!-- End Grid -->
+</div>
+<!-- End Card Section -->
       </div>
     </ion-content>
   </ion-page>
